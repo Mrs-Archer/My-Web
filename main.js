654e1acaed1cfe6aef728a1ada3531f568b4eb87
Mrs-Archer <i.juskevic@gmail.com>
2021-06-02 22:10:09 +0100

    active-nav-bar

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 document.getElementById("home-hero").classList.add("active");
+
+// Get the container element
+var btnContainer = document.getElementById("navigation-bar");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("li");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
